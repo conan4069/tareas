5d74889469defb100a59f675027e16954d483fa6
conan4069 <mevr02@gmail.com>
2019-01-09 15:27:12 -0400

    Condicinales

--- src/components/base/condicionales.vue
+++ src/components/base/condicionales.vue
@@ -61,7 +61,134 @@
           Contar
         </v-btn>
         Una vez mostrado este pequeño ejemplo seguiremos con nuestra tarea
-        ahora crearemos una siguiente 
+        ahora crearemos un siguiente v-expansion-content
+        <pre class="language-js">
+          <code style="box-shadow: none;">
+            //Bueno en nuestra data agregaremos las siguientes variables
+            data(){
+              return {
+               //...
+               text:"",
+               indextask:"",
+               fab:false,
+               dialog:false,
+              }
+            },
+            computed:{
+              //Ahora estan los campos calculados es cuando queremos mostrar una variable
+              //calculada o dinamica
+              totalSubtask(){
+                return this.task.subtask.length
+              },
+              completedSubtask(){
+                return this.task.subtask.filter(o => o.done === true ).length
+              }
+            },
+            methods:{
+              //En esta seccion como su nombre lo dice crearemos
+              //Todos los metodos que usaremos en nuestro componente
+              save(){
+                this.dialog = false
+                if(this.indextask === ''){
+                  this.task.subtask.push({
+                    'task': this.text,
+                    'done': false
+                  })
+                }
+                else{
+                  this.task.subtask[this.indextask].task = this.text
+                  this.indextask = ""
+                }
+                this.text = ""
+              },
+              editSubTask(index){
+                if(!this.task.subtask[index].done){
+                  this.text = this.task.subtask[index].task
+                  this.indextask = index
+                  this.dialog = true
+                }
+              },
+              removeSubTask(index){
+                this.task.subtask.splice(index,1)
+              }
+            },
+          </code>
+        </pre>
+        <pre class="language-html">
+          <code style="box-shadow: none;">
+            &lt;v-expansion-panel-content>
+              &lt;div slot="header">
+                &lt;p class="text-md-center display-1 blue-grey--text darken-3">
+                  &lt;b>Información extra&lt;/b>
+                &lt;/p>
+              &lt;/div>
+              &lt;v-layout row wrap>
+                &lt;v-flex xs12>
+                  &lt;p class="text-md-center title blue-grey--text darken-3">
+                    &lt;b>Sub Tareas&lt;/b>
+                  &lt;/p>
+                &lt;/v-flex>
+                &lt;v-list-tile @click="dialog = !dialog" class="add">
+                  &lt;v-list-tile-content class="text-md-center">
+                    &lt;v-list-tile-sub-title>
+                      &lt;v-icon>fa-plus&lt;/v-icon>
+                      &lt;span class="caption blue-grey--text darken-2" 
+                        style="z-index:4;position:absolute;bottom: 0px;right:2rem;"
+                      >
+                        { { completedSubtask } }/{ { totalSubtask } } 
+                        &lt;!-- Debes eliminar el espacio en blanco entre las llaves -->
+                      &lt;/span>
+                    &lt;/v-list-tile-sub-title>
+                  &lt;/v-list-tile-content>
+                &lt;/v-list-tile>
+              &lt;/v-layout>
+            &lt;/v-expansion-panel-content>
+            &lt;v-dialog
+                v-model="dialog"
+                max-width="800px"
+              >
+              &lt;v-card>
+                &lt;v-card-title class="headline">Añadir SubTarea&lt;/v-card-title>
+                &lt;v-card-text>
+                  &lt;v-form @submit.prevent="save()">
+                    &lt;v-text-field
+                      autofocus
+                      v-model="text"
+                      label="Tarea"
+                    >
+                    &lt;/v-text-field>
+                  &lt;/v-form>
+                &lt;/v-card-text>
+
+                &lt;v-card-actions>
+                  &lt;v-spacer>&lt;/v-spacer>
+
+                  &lt;v-btn
+                    color="blue-grey darken-1"
+                    flat="flat"
+                    @click="dialog = false"
+                  >
+                    Cancel
+                  &lt;/v-btn>
+
+                  &lt;v-btn
+                    color="blue-grey darken-1"
+                    flat="flat"
+                    @click="save()"
+                  >
+                    &lt;span v-if="indextask === ''">Añadir&lt;/span>
+                    &lt;span v-else>Editar&lt;/span>
+                  &lt;/v-btn>
+                &lt;/v-card-actions>
+              &lt;/v-card>
+            &lt;/v-dialog>
+          </code> 
+        </pre>
+        Bueno con esto hemos creado una pantalla desplegable para crear subtareas subtareas,
+        y esta misma pantalla nos servira para editar, creamos todos los metodos que simplemente
+        usaremos para crud de las subtareas, creamos dos campos computados <b>completedSubtask</b> 
+        veremos la cantidad de tareas completadas con <b>totalSubtask</b> Sabremos la cantidad
+        total de subtareas 
       </span>
     </v-flex>
   </v-layout>
